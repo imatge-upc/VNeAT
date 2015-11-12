dccdd8f524de7f56238bac3f07b2b2e9174346f7
Asier Aduriz <asier.aduber@gmail.com>
2015-11-12 20:00:58 +0100

    Added orthogonalize method to GLM class

--- nonlinear/patterns/curve_fit.py
+++ nonlinear/patterns/curve_fit.py
@@ -19,6 +19,45 @@ class FunctionWrapper:
 		return eval(func)
 
 
+class GLM:
+	def __init__(self, xdata, ydata):
+		if len(xdata.shape) == 1:
+			self.xdata = nparray([xdata.copy()])
+		else:
+			self.xdata = xdata.copy()
+		assert xdata.shape[1] == ydata.shape[0]
+		self.num_regressors = self.xdata.shape[0]
+		self.ydata = ydata.copy()
+
+	@staticmethod
+	def pred_function(xdata, *args):
+		return sum(xdata[i]*args[i] for i in range(min(xdata.shape[0], len(args))))
+
+	def orthogonalize(self):
+		orig_ydata = self.ydata
+		orig_num_regressors = self.num_regressors
+		try:
+			for i in range(1, orig_num_regressors):
+				self.num_regressors = i
+				self.ydata = self.xdata[i, :]
+				self.optimize()
+				self.xdata[i, :] -= self.pred_function(self.xdata, *self.opt_params)
+				del self.opt_params, self.opt_params_cov
+		except Exception as e:
+			raise e
+		finally:
+			self.num_regressors = orig_num_regressors
+			self.ydata = orig_ydata
+
+	def optimize(self, p0=None, sigma=None, absolute_sigma=False, check_finite=True, **kw):
+		num_params = self.num_regressors
+		fw = FunctionWrapper(GLM.pred_function, self.xdata[:num_params, :])
+		self.opt_params, self.opt_params_cov = curve_fit(fw.wrap(num_params), fw, self.ydata, p0, sigma, absolute_sigma, check_finite, **kw)
+
+
+
+# --------  Dead code goes here (ignore)  --------
+
 #	def add_params(func, num_added_params):
 #		num_orig_params = func.func_code.co_argcount
 #		new_nparams = num_orig_params + num_added_params
@@ -31,42 +70,6 @@ class FunctionWrapper:
 #		function += 'x' + str(new_nparams - 1) + ')'
 #		return eval(function)
 
-
-class GLM:
-	def __init__(self, xdata, ydata):
-		if len(xdata.shape) == 1:
-			self.xdata = nparray([xdata.copy()])
-		else:
-			self.xdata = xdata.copy()
-		self.num_regressors = self.xdata.shape[0]
-		self.ydata = ydata.copy()
-
-	@staticmethod
-	def pred_function(xdata, *args):
-		return sum(xdata[i]*args[i] for i in range(min(xdata.shape[0], len(args))))
-
-		'''
-	def orthogonalize(self):
-		if self.num_regressors > 1:
-			orig_ydata = self.ydata
-			orig_num_regressors = self.num_regressors
-			self.ydata = self.xdata([self.num_regressors - 1, :])
-			try:
-				self.num_regressors -= 1
-				self.optimize()
-				self.xdata[self.num_regressors, :] -= self.pred_function(self.xdata, *self.opt_params)
-			except Exception as e:
-				raise e
-			finally:
-				self.num_regressors = orig_num_regressors
-				self.ydata = orig_ydata
-		'''
-
-	def optimize(self, p0=None, sigma=None, absolute_sigma=False, check_finite=True, **kw):
-		num_params = self.num_regressors
-		fw = FunctionWrapper(GLM.pred_function, self.xdata[:num_params, :])
-		self.opt_params, self.opt_params_cov = curve_fit(fw.wrap(num_params), fw, self.ydata, p0, sigma, absolute_sigma, check_finite, **kw)
-
 #	def glm(xdata, ydata, p0=None, sigma=None, absolute_sigma=False, check_finite=True, **kw):
 #		# xdata dimensions = (k, M) or just (M) in case it is 1-dimensional
 #		# ydata dimensions = (M)
@@ -74,7 +77,6 @@ class GLM:
 #		if len(xdata.shape) == 1:
 #			xdata = nparray([xdata])
 #			# convert dimension (M) to (1, M) so that we can treat this case equally
-#	
 #	
 #	
 #		def pred_function(xdata, *args):
@@ -86,4 +88,24 @@ class GLM:
 #	
 #		return curve_fit(fw.wrap(num_params), fw, ydata, p0, sigma, absolute_sigma, check_finite, **kw)
 
+
+
+#	INSIDE GLM CLASS:
+#
+#	def orthogonalize(self):
+#		if self.num_regressors > 1:
+#			orig_ydata = self.ydata
+#			orig_num_regressors = self.num_regressors
+#			self.ydata = self.xdata[self.num_regressors - 1, :]
+#			try:
+#				self.num_regressors -= 1
+#				self.orthogonalize()
+#				self.optimize()
+#				self.xdata[self.num_regressors, :] -= self.pred_function(self.xdata, *self.opt_params)
+#				del self.opt_params, self.opt_params_cov
+#			except Exception as e:
+#				raise e
+#			finally:
+#				self.num_regressors = orig_num_regressors
+#				self.ydata = orig_ydata
 	
