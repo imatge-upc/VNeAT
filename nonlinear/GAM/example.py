a468d0ee1f65f0841615e6bac47110bf0ed480bd
acasamitjana <acasamitjana@upc.edu>
2015-11-19 12:18:49 +0100

    Example GAM

--- nonlinear/GAM/example.py
+++ nonlinear/GAM/example.py
@@ -9,100 +9,63 @@ import numpy.random as R
 import matplotlib.pyplot as plt
 
 from statsmodels.sandbox.gam import AdditiveModel
-from statsmodels.sandbox.gam import Model as GAM #?
 from statsmodels.genmod.families import family
 from statsmodels.genmod.generalized_linear_model import GLM
 
 standardize = lambda x: (x - x.mean()) / x.std()
-demean = lambda x: (x - x.mean())
 nobs = 150
 x1 = R.standard_normal(nobs)
 x1.sort()
 x2 = R.standard_normal(nobs)
 x2.sort()
-y = R.standard_normal((nobs,))
+y= 0.5*R.standard_normal(nobs)#np.zeros(nobs)#
 
-f1 = lambda x1: (x1 + x1**2 - 3 - 1 * x1**3 + 0.1 * np.exp(-x1/4.))
-f2 = lambda x2: (x2 + x2**2 - 0.1 * np.exp(x2/4.))
+f1 = lambda x1: (1 + x1 + x1**2 )
+f2 = lambda x2: (1 + x2 - x2**2)
+
+
+
 z = standardize(f1(x1)) + standardize(f2(x2))
-z = standardize(z) * 2 # 0.1
+z = standardize(z) #* 2 # 0.1
 
 y += z
 d = np.array([x1,x2]).T
 
-
-if example == 1:
-    print("normal")
-    m = AdditiveModel(d)
-    m.fit(y)
-    x = np.linspace(-2,2,50)
-
-    print(m)
-
-    y_pred = m.results.predict(d)
-    plt.figure()
-    plt.plot(y, '.')
-    plt.plot(z, 'b-', label='true')
-    plt.plot(y_pred, 'r-', label='AdditiveModel')
-    plt.legend()
-    plt.title('gam.AdditiveModel')
+#Estimation
+m = AdditiveModel(d)
+m.fit(y)
+x = np.linspace(-2,2,50)
+y_pred=m.results.predict(d);
+print(y-y_pred)
 
 
-import scipy.stats, time
-
-if example == 2:
-    print("binomial") 
-    f = family.Binomial()
-    b = np.asarray([scipy.stats.bernoulli.rvs(p) for p in f.link.inverse(y)])
-    b.shape = y.shape
-    m = GAM(b, d, family=f)
-    toc = time.time()
-    m.fit(b)
-    tic = time.time()
-    print(tic-toc)
-	
-    y_pred = m.results.predict(d)
-    plt.figure()
-    plt.plot(y, '.')
-    plt.plot(z, 'b-', label='true')
-    plt.plot(y_pred, 'r-', label='AdditiveModel')
-    plt.legend()
-    plt.title('gam.AdditiveModel')
-
-	
-if example == 3:
-    print("Poisson")
-    f = family.Poisson()
-    y = y/y.max() * 3
-    yp = f.link.inverse(y)
-    p = np.asarray([scipy.stats.poisson.rvs(p) for p in f.link.inverse(y)], float)
-    p.shape = y.shape
-    m = GAM(p, d, family=f)
-    toc = time.time()
-    m.fit(p)
-    tic = time.time()
-    print(tic-toc)
-    
-    y_pred = m.results.predict(d)
-    plt.figure()
-    plt.plot(y, '.')
-    plt.plot(z, 'b-', label='true')
-    plt.plot(y_pred, 'r-', label='GAM')
-    plt.legend()
-    plt.title('gam.GAM')
-
-
+#Plots
 plt.figure()
-plt.plot(x1, standardize(m.smoothers[0](x1)), 'r')
-plt.plot(x1, standardize(f1(x1)), linewidth=2)
+plt.plot(x1, f1(x1), 'r')
+plt.plot(x2, f2(x2), 'b')
+ 
+ 
 plt.figure()
-plt.plot(x2, standardize(m.smoothers[1](x2)), 'r')
-plt.plot(x2, standardize(f2(x2)), linewidth=2)
-
+plt.plot(y, '.')
+plt.plot(z, 'b-', label='true')
+plt.plot(y_pred, 'r-', label='AdditiveModel')
+plt.legend()
+plt.title('gam.AdditiveModel')
+ 
+plt.figure()
+plt.plot(x1,y-standardize(f2(x2)), 'k.')
+plt.plot(x1, standardize(m.smoothers[0](x1)), 'r-', label='AdditiveModel')
+plt.plot(x1, standardize(f1(x1)),label='true', linewidth=2)
+  
+plt.figure()
+plt.plot(x2,y-standardize(f1(x1)), 'k.')
+plt.plot(x2,   standardize(m.smoothers[1](x2)), 'r-', label='AdditiveModel')
+plt.plot(x2, standardize(f2(x2)), label='true', linewidth=2)
 
 
 
 plt.show()
+
 
 
 
