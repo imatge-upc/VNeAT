64afacbe7fe317653e0179f2bdea4620d4a7a11b
acasamitjana <adria.casamitjana@upc.edu>
2016-01-19 19:36:08 +0100

    Commit .gitignore

--- nonlinear2/GAM.py
+++ nonlinear2/GAM.py
@@ -1,27 +1,131 @@
-from numpy import array as nparray
+from CurveFitting import AdditiveCurveFitter
+from numpy import zeros, array as nparray
 import numpy as np
-from statsmodels.sandbox.nonparametric import kernels
-from scipy.interpolate import UnivariateSpline
-import patsy as pt
+
+class GAM(AdditiveCurveFitter):
+    '''Additive model with non-parametric, smoothed components
+
+    Parameters
+    ----------
+    exog : ndarray
+    smoothers : None or list of smoother instances
+        smoother instances not yet checked
+    weights : None or ndarray
+    family : None or family instance
+        I think only used because of shared results with GAM and subclassing.
+        If None, then Gaussian is used.
+    '''
+
+    def __init__(self, exog, smoothers=None, weights=None, family=None):
+        self.exog = exog
+        if not weights is None:
+            self.weights = weights
+        else:
+            self.weights = np.ones(self.exog.shape[0])
+
+        self.smoothers = smoothers
+
+        if family is None:
+            self.family = families.Gaussian()
+        else:
+            self.family = family
+        #self.family = families.Gaussian()
+
+    def _iter__(self):
+        self.iter = 0
+        self.dev = np.inf
+        return self
+
+    def cont(self):
+        self.iter += 1 #moved here to always count, not necessary
+
+        curdev = (((self.results.Y - self.results.predict(self.exog))**2) * self.weights).sum()
+
+        if self.iter > self.maxiter: #kill it, no max iterationoption
+            return False
+        if ((self.dev - curdev) / curdev) < self.rtol:
+            self.dev = curdev
+            return False
+
+        self.dev = curdev
+        return True
+
+    # def df_resid(self):
+    #     return self.results.Y.shape[0] - np.array([self.smoothers[i].df_fit() for i in range(self.exog.shape[1])]).sum()
+    #
+    # def estimate_scale(self):
+    #     return ((self.results.Y - self.results(self.exog))**2).sum() / self.df_resid()
+
+    def fit(self, Y, rtol=1.0e-06, maxiter=30):
+        self.rtol = rtol
+        self.maxiter = maxiter
+        self._iter__()
+        mu = 0
+        alpha = (Y * self.weights).sum() / self.weights.sum()
+
+        offset = np.zeros(self.smoothers.__len__(), np.float64)
+
+        for i in range(self.smoothers.__len__()):
+            r=Y - alpha - mu
+            self.smoothers[i].smooth(Y - alpha - mu,
+                                     weights=self.weights)
+            f_i_pred = self.smoothers[i].predict()
+            offset[i] = (f_i_pred * self.weights).sum() / self.weights.sum()
+            f_i_pred -= f_i_pred.sum()/Y.shape[0]
+            mu += f_i_pred
+
+        self.results = Results(Y, alpha, self.exog, self.smoothers, self.family, offset)
+
+        while self.cont():
+            self.results = self.next()
+
+        if self.iter >= self.maxiter:
+            warnings.warn(iteration_limit_doc, IterationLimitWarning)
+
+        return self.results
+
+    def next(self):
+        _results = self.results
+        Y = self.results.Y
+        mu = _results.predict(self.exog)
+        offset = np.zeros(self.smoothers.__len__(), np.float64)
+        alpha = (Y * self.weights).sum() / self.weights.sum()
+        for i in range(self.smoothers.__len__()):
+            tmp = self.smoothers[i].predict()
+            bad = np.isnan(Y - alpha - mu + tmp).any()
+            if bad: #temporary assert while debugging
+                print(Y, alpha, mu, tmp)
+                raise ValueError("nan encountered")
+            #self.smoothers[i].smooth(Y - alpha - mu + tmp,
+            self.smoothers[i].smooth(Y - mu + tmp,
+                                     weights=self.weights)
+            f_i_pred = self.smoothers[i].predict() #fit the values of previous smooth/fit
+            self.results.offset[i] = (f_i_pred*self.weights).sum() / self.weights.sum()
+            f_i_pred -= f_i_pred.sum()/Y.shape[0]
+            if DEBUG:
+                print(self.smoothers[i].params)
+            mu += f_i_pred - tmp
+        offset = self.results.offset
+        return Results(Y, alpha, self.exog, self.smoothers, self.family, offset)
+
 
 class Smoother:
     # smoothers should have the method 'smooth'.
 
     def __init__(self, xdata=None, smoother=None):
-        if xdata is None:
-            self.xdata = np.array([])
+        if xdata is None and smoother is None:
+            self.xdata = nparray([])
             self.num_reg = 0
             self.smoother = []
-        elif smoother is None:
-            self.xdata = nparray(xdata, dtype=float)
-            self.num_reg = self.xdata.shape[1]
-            self.smoother = self.set_polySmoother(self.xdata,1)  # Set the identity as smoother
-        else:
+        elif xdata is not None and smoother is not None:
             self.xdata=nparray(xdata, dtype=float)
             self.smoother=smoother
             self.num_reg = len(smoother)
+        else:
+            raise TypeError('You should specifify a smoother for each regressor and viceversa')
 
     def set_polySmoother(self, xdata, d=None):
+
         xdata=nparray(xdata, dtype=float)
         if d is None:
             d = 1
@@ -31,14 +135,10 @@ class Smoother:
         else:
             self.xdata=np.c_[self.xdata, xdata]
 
-        if xdata.ndim == 1:
-            nreg=1
-            self.smoother.append(PolySmoother(d, xdata.copy()))
-        else:
-            nreg=xdata.shape[1]
-            self.smoother.append([PolySmoother(d, xdata[:, i].copy()) for i in range(nreg)])
+        for i in range(xdata.shape[1]):
+            self.smoother.append(PolySmoother(d, xdata[:, i].copy()))
 
-        self.num_reg =self.num_reg + nreg
+        self.num_reg =self.num_reg + xdata.shape[1]
 
 
     def set_splinesSmoother(self,xdata,d=None,s=None):
@@ -261,7 +361,6 @@ class PolySmoother(object):
         self.params = np.squeeze(self.coef)
 
 
-
 class Gaussian:
     """
     Gaussian (Normal) Kernel
@@ -285,4 +384,70 @@ class Gaussian:
         v = np.sum(multiply(ys, exp(multiply(square(divide(subtract(xs, x),
                                                           self.h)), -0.5))))
         return v/w
+
+
+class Results(object):
+
+    def __init__(self, Y, alpha, exog, smoothers, family, offset):
+
+        self.nobs = Y.shape[0]
+        self.k_vars = smoothers.__len__()
+        self.Y = Y
+        self.alpha = alpha
+        self.smoothers = smoothers
+        self.offset = offset
+        self.family = family
+        self.exog = exog
+        self.offset = offset
+        self.mu = self.linkinversepredict(exog)
+
+    def linkinversepredict(self, exog):  #For GAM (link function)
+        '''expected value ? check new GLM, same as mu for given exog
+        '''
+        return self.family.link.inverse(self.predict(exog))
+
+    def predict(self, exog):
+        '''predict response, sum of smoothed components
+        TODO: What's this in the case of GLM, corresponds to X*beta ?
+        '''
+        #note: sum is here over axis=0,
+        #TODO: transpose in smoothed and sum over axis=1
+
+        #BUG: there is some inconsistent orientation somewhere
+        #temporary hack, won't work for 1d
+        #print dir(self)
+        #print 'self.nobs, self.k_vars', self.nobs, self.k_vars
+        exog_smoothed = self.smoothed(exog)
+        #print 'exog_smoothed.shape', exog_smoothed.shape
+        if exog_smoothed.shape[0] == self.k_vars:
+            import warnings
+            warnings.warn("old orientation, colvars, will go away",
+                          FutureWarning)
+            return np.sum(self.smoothed(exog), axis=0) + self.alpha
+        if exog_smoothed.shape[1] == self.k_vars:
+            return np.sum(exog_smoothed, axis=1) + self.alpha
+        else:
+            raise ValueError('shape mismatch in predict')
+
+    def smoothed(self, exog=None): #ADRIA changed this from exog -- exog==None
+        if exog is None:
+            exog=self.exog
+        '''get smoothed prediction for each component
+
+        '''
+        #bug: with exog in predict I get a shape error
+        #print 'smoothed', exog.shape, self.smoothers[0].predict(exog).shape
+        #there was a mistake exog didn't have column index i
+
+
+        if exog.ndim == 1:
+            return np.array([self.smoothers[0].predict(exog) + self.offset])
+        else:
+            return np.array([self.smoothers[i].predict(exog[:,i]) + self.offset[i]
+                            for i in range(self.smoothers.__len__())]).T
+        #ADRIA changed this: BEFORE: return np.array([self.smoothers[i].predict(exog[:,i]) + self.offset[i]
+            #shouldn't be a mistake because exog[:,i] is attached to smoother, but
+            #it is for different exog
+            #return np.array([self.smoothers[i].predict() + self.offset[i]
+                         # for i in range(self.smoothers.__len__())]).T
 
