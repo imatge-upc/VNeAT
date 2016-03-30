e6128ee1033c7cc136d9125eb8685d060945975e
Santi Puch Giner <santiago.puch@alu-etsetb.upc.edu>
2016-03-31 01:25:30 +0200

    Run multiple simulations with different C and epsilon params

--- nonlinear2/FPMAlfaSVR.py
+++ nonlinear2/FPMAlfaSVR.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
 
     print 'Obtaining data from Excel file...'
 
-    """
+
     DATA_DIR = join("C:\\", "Users", "santi", "Documents", "Santi", "Universitat", "TFG", "Data", "nonlinear_data", "Nonlinear_NBA_15_corrected")
     EXCEL_FILE = join("C:\\", "Users", "santi", "Documents", "Santi", "Universitat", "TFG", "Data", "nonlinear_data", "work_DB_CSF.R1.final.xls")
     RESULTS_DIR = join("results", "PSVR")
@@ -20,7 +20,7 @@ if __name__ == "__main__":
     DATA_DIR = join("/", "imatge", "spuch", "data-neuroimatge", "Nonlinear_NBA_15_corrected")
     EXCEL_FILE = join("/", "imatge", "spuch", "data-neuroimatge", "work_DB_CSF.R1.final.xls")
     RESULTS_DIR = join("/", "imatge", "spuch", "work", "neuro", "PolySVR")
-
+    """
 
     filenames = filter(isfile, map(lambda elem: join(DATA_DIR, elem), listdir(DATA_DIR)))
     ar = [basename(fn).split('_')[1][:-4] for fn in filenames]
@@ -51,38 +51,47 @@ if __name__ == "__main__":
             )
         )
 
-    print 'Initializing PolySVR Processor...'
-    user_defined_parameters = (0, 0, 250.0, 0.001, 3)
-    psvr = PSVR(subjects, regressors = [Subject.ADCSFIndex], user_defined_parameters=user_defined_parameters)
+    # Exhaustive use of parameters
+    C_list = [100, 250, 500]
+    epsilon_list = [0.001, 0.0001]
 
-    print 'Processing data...'
-    results = psvr.process(n_jobs=4, mem_usage=50)
+    for C in C_list:
+        for epsilon in epsilon_list:
+            print 'Initializing PolySVR Processor with C = ', str(C), 'epsilon = ', str(epsilon) + ' ...'
+            user_defined_parameters = (0, 0, C, epsilon, 3)
+            psvr = PSVR(subjects, regressors = [Subject.ADCSFIndex], user_defined_parameters=user_defined_parameters)
 
-    print 'Saving results to files...'
+            print 'Processing data...'
+            results = psvr.process(n_jobs=8, mem_usage=50)
 
-    affine = array(
-            [[ -1.50000000e+00,   0.00000000e+00,   0.00000000e+00,   9.00000000e+01],
-             [  1.99278252e-16,   1.50000000e+00,   2.17210575e-16,  -1.26000000e+02],
-             [ -1.36305018e-16,  -1.38272305e-16,   1.50000000e+00,  -7.20000000e+01],
-             [  0.00000000e+00,   0.00000000e+00,   0.00000000e+00,   1.00000000e+00]]
-    )
+            print 'Saving results to files...'
 
-    niiFile = nib.Nifti1Image
+            affine = array(
+                    [[ -1.50000000e+00,   0.00000000e+00,   0.00000000e+00,   9.00000000e+01],
+                     [  1.99278252e-16,   1.50000000e+00,   2.17210575e-16,  -1.26000000e+02],
+                     [ -1.36305018e-16,  -1.38272305e-16,   1.50000000e+00,  -7.20000000e+01],
+                     [  0.00000000e+00,   0.00000000e+00,   0.00000000e+00,   1.00000000e+00]]
+            )
 
-    nib.save(niiFile(results.correction_parameters, affine), join(RESULTS_DIR, 'fpmalfa_psvr_cparams.nii'))
-    nib.save(niiFile(results.regression_parameters, affine), join(RESULTS_DIR, 'fpmalfa_psvr_rparams.nii'))
-    nib.save(niiFile(results.fitting_scores, affine), join(RESULTS_DIR, 'fpmalfa_psvr_fitscores.nii'))
+            niiFile = nib.Nifti1Image
 
-    with open(join(RESULTS_DIR, 'fpmalfa_psvr_userdefparams.txt'), 'wb') as f:
-        f.write(str(psvr.user_defined_parameters) + '\n')
+            filename = 'psvr_C' + str(C) + '_eps' + str(epsilon) + '_'
+            nib.save(niiFile(results.correction_parameters, affine), join(RESULTS_DIR, filename + 'cparams.nii'))
+            nib.save(niiFile(results.regression_parameters, affine), join(RESULTS_DIR, filename + 'rparams.nii'))
+            nib.save(niiFile(results.fitting_scores, affine), join(RESULTS_DIR, filename + 'fitscores.nii'))
 
-    print 'Obtaining, filtering and saving z-scores and labels to display them...'
-    for fit_threshold in [0.99, 0.995, 0.999]:
-        print '    Fitting-threshold set to', fit_threshold, '; Computing z-scores and labels...'
-        z_scores, labels = psvr.fit_score(results.fitting_scores, fit_threshold = fit_threshold, produce_labels = True)
+            with open(join(RESULTS_DIR, filename + 'userdefparams.txt'), 'wb') as f:
+                f.write(str(psvr.user_defined_parameters) + '\n')
 
-        print '    Saving z-scores and labels to file...'
-        nib.save(niiFile(z_scores, affine), join(RESULTS_DIR, 'fpmalfa_psvr_zscores_' + str(fit_threshold) + '.nii'))
-        nib.save(niiFile(labels, affine), join(RESULTS_DIR, 'fpmalfa_psvr_labels_' + str(fit_threshold) + '.nii'))
+            """
+            print 'Obtaining, filtering and saving z-scores and labels to display them...'
+            for fit_threshold in [0.99, 0.995, 0.999]:
+                print '    Fitting-threshold set to', fit_threshold, '; Computing z-scores and labels...'
+                z_scores, labels = psvr.fit_score(results.fitting_scores, fit_threshold = fit_threshold, produce_labels = True)
 
-    print 'Done.'
+                print '    Saving z-scores and labels to file...'
+                nib.save(niiFile(z_scores, affine), join(RESULTS_DIR, filename + 'zscores_' + str(fit_threshold) + '.nii'))
+                nib.save(niiFile(labels, affine), join(RESULTS_DIR, filename + 'labels_' + str(fit_threshold) + '.nii'))
+
+            print 'Done.'
+            """
