e767d4255c1e7edef5dd4a70ac25f111ed0c6424
acasamitjana <adria.casamitjana@upc.edu>
2016-04-07 17:34:05 +0200

    remove user_paths.py

--- user_paths.py
+++
@@ -1,5 +0,0 @@
-from os.path import join
-
-DATA_DIR = join('/','imatge','spuch','data-neuroimatge', 'Nonlinear_NBA_15')
-CORRECTED_DIR = join('/','imatge','spuch','data-neuroimatge', 'Nonlinear_NBA_15_corrected')
-EXCEL_FILE = join('/','imatge','spuch','data-neuroimatge', 'work_DB_CSF.R1.final.xls')
