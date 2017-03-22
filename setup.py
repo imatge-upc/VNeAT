97364cd951e38a6e595361c214580f780785bfe5
Santi Puch Giner <santipuch590@gmail.com>
2017-03-22 01:02:51 +0100

    Fix setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from distutils.core import setup
 setup(
     name='VNeAT',
     version='0.0.0',
-    packages=['src', 'src.Utils', 'src.Fitters', 'src.FitScores', 'src.Processors', 'src.Visualization',
-              'src.CrossValidation'],
+    packages=['vneat', 'vneat.Utils', 'vneat.Fitters', 'vneat.FitScores', 'vneat.Processors', 'vneat.Visualization',
+              'vneat.CrossValidation'],
     scripts=['vneat-compare_statistical_maps.py', 'vneat-compute_fitting.py', 'vneat-compute_statistical_maps.py',
              'vneat-generate_user_parameters.py', 'vneat-search_hyperparameters.py', 'vneat-show_curves.py',
              'vneat-show_data_distribution.py', 'vneat-show_visualizer.py'],
